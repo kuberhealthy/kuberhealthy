34e45472c93472792cb4d96827fccbda2db024fe
integrii <integrii@gmail.com>
2018-10-26 12:08:16 -0700

    implement some go modules stuff for #40

--- pkg/khstatecrd/api.go
+++ pkg/khstatecrd/api.go
@@ -9,7 +9,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-package khstatecrd
+package khstatecrd // import "github.com/Comcast/kuberhealthy/pkg/khstatecrd"
 
 import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
