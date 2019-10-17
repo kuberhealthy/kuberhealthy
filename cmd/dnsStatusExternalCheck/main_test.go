0cd9a3e7e39ba565cd596191b374274911e9b73e
joshulyne <joshpark118@gmail.com>
2019-10-17 16:52:46 -0700

    Initial commit

---
+++ cmd/dnsStatusExternalCheck/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+)
+
+const kubeConfigFile = "~/.kube/config"
+
+func TestDnsStatusChecker(t *testing.T) {
+	c := New([]string{})
+	err := c.doChecks()
+	if err != nil {
+		t.Fatal(err)
+	}
+	up, errors := c.CurrentStatus()
+	t.Log("up:", up)
+	t.Log("errors:", errors)
+	err = c.Shutdown()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
