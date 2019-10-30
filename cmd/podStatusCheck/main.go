70edbf4905d177c20f8f3cd78c7c40812a6a45e5
rukatm <mike.kurashima@gmail.com>
2019-10-30 14:22:56 -0700

    testing checks against the container levels

--- cmd/podStatusCheck/main.go
+++ cmd/podStatusCheck/main.go
@@ -59,13 +59,62 @@ func main() {
 	}
 }
 
-// findPodsNotRunning gets a list of pods that are older than 10 minutes and contain all Pod status phases
+// testing a different way to find failed container statuses
+// seen instances where Pods that are "Running" but are in "Crashloopbackoff" due to container "Waiting"
+// need to hit "Running" statuses in both Pod and Container
+// possible pod status phases
+// Pending | Running | Succeeded | Failed | Unknown
+// possible container status phases
+// Waiting | Running | Terminated
+
+
+func findPodsNotRunning(client *kubernetes.Clientset) ([]string, error) {
+
+	var failures []string
+
+	pods, err := client.CoreV1().Pods(namespace).List(metav1.ListOptions{})
+	if err != nil {
+		return failures, err
+	}
+    // start iteration over pods
+	for _, pod := range pods.Items {
+		// check if the pod age is over 10 minutes
+		if time.Now().Sub(pod.CreationTimestamp.Time).Minutes() < 10 {
+			continue
+		}
+		// filter out pods that are not Running
+		if pod.Status.Phase != v1.PodRunning {
+			log.Infoln("Found pod: ", pod.Name)
+			failures = append(failures, pod.Name+" has bad state "+string(pod.Status.Phase))
+			continue
+		}
+		// for the pods that are Running we start checking container statuses within them
+		var containersNotReady bool
+		for _, container := range pod.Status.ContainerStatuses {
+			if !container.Ready	{
+				log.Infoln("Found bad container: ", container.Name, "in", pod.Name)
+				containersNotReady = true
+				break
+			}
+		}
+		if containersNotReady {
+		    failures = append(failures, pod.Name+" has containers that are not ready.")
+		}
+	}
+log.Infoln("Here are the list of failed pods: ", failures)
+
+return failures, nil
+
+}
+
+
+/* findPodsNotRunning gets a list of pods that are older than 10 minutes and contain all Pod status phases
 // that are NOT healthy/Running.
 func findPodsNotRunning(client *kubernetes.Clientset) ([]string, error) {
 
-	log.Infoln("Finding pods not running in namespace: ", namespace)
-
 	var failures []string
+
+	log.Infoln("Finding pods not running in namespace: ", namespace)
 
 	pods, err := client.CoreV1().Pods(namespace).List(metav1.ListOptions{})
 
@@ -93,3 +142,4 @@ func findPodsNotRunning(client *kubernetes.Clientset) ([]string, error) {
 	return failures, nil
 
 }
+*/
