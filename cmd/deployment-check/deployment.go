e0563b9a61a01e2bdbd8c3e84e0d3589374b800a
Jonathan Phan <jonathan_phan@comcast.com>
2019-11-06 10:18:16 -0800

    update deployment check deployment and svc names; remove random comment from deploy yaml

--- cmd/deployment-check/deployment.go
+++ cmd/deployment-check/deployment.go
@@ -29,7 +29,7 @@ import (
 
 const (
 	// Default deployment values.
-	defaultLabelKey        = "kh-deployment-timestamp"
+	defaultLabelKey        = "deployment-timestamp"
 	defaultLabelValueBase  = "unix-"
 	defaultMinReadySeconds = 5
 
