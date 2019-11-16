88e11c6bb75ea0adcdc7b86b96487202ca88a15e
Jonathan Phan <jonathan_phan@comcast.com>
2019-11-15 17:10:46 -0800

    minor fixes to templating script and files

--- deploy/helm/create-flat-files.sh
+++ deploy/helm/create-flat-files.sh
@@ -4,7 +4,7 @@ echo "Creating flat kuberhealthy.yaml"
 helm template kuberhealthy > ../kuberhealthy.yaml
 
 echo "Creating flat kuberhealthy-prometheus.yaml"
-helm template kuberhealthy kuberhealthy --set global.prometheus.enabled=true global.prometheus.enableScraping=true global.prometheus.serviceMonitor=true global.prometheus.enableAlerting=true > ../kuberhealthy-prometheus.yaml
+helm template kuberhealthy kuberhealthy --set prometheus.enabled=true prometheus.enableScraping=true prometheus.serviceMonitor=true prometheus.enableAlerting=true > ../kuberhealthy-prometheus.yaml
 
 echo "Creating flat kuberhealthy-prometheus-operator.yaml"
-helm template kuberhealthy kuberhealthy --set global.prometheus.enabled=true global.prometheus.serviceMonitor=true > ../kuberhealthy-prometheus-operator.yaml
+helm template kuberhealthy kuberhealthy --set prometheus.enabled=true prometheus.serviceMonitor=true > ../kuberhealthy-prometheus-operator.yaml
