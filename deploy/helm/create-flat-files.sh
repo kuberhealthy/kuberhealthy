1ed56961a52300083820e152f50f805d1b6c582a
Hanfei Shen <qqshfox@gmail.com>
2019-11-21 14:15:18 +0800

    fix flat files

--- deploy/helm/create-flat-files.sh
+++ deploy/helm/create-flat-files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Creating flat kuberhealthy.yaml"
-helm template kuberhealthy > ../kuberhealthy.yaml
+helm template --namespace kuberhealthy kuberhealthy > ../kuberhealthy.yaml
 
 echo "Creating flat kuberhealthy-prometheus.yaml"
-helm template kuberhealthy kuberhealthy --set prometheus.enabled=true prometheus.enableScraping=true prometheus.serviceMonitor=true prometheus.enableAlerting=true > ../kuberhealthy-prometheus.yaml
+helm template --namespace kuberhealthy kuberhealthy kuberhealthy --set prometheus.enabled=true --set prometheus.enableScraping=true --set prometheus.enableAlerting=true > ../kuberhealthy-prometheus.yaml
 
 echo "Creating flat kuberhealthy-prometheus-operator.yaml"
-helm template kuberhealthy kuberhealthy --set prometheus.enabled=true prometheus.serviceMonitor=true > ../kuberhealthy-prometheus-operator.yaml
+helm template --namespace kuberhealthy kuberhealthy kuberhealthy --set prometheus.enabled=true --set prometheus.enableScraping=true --set prometheus.enableAlerting=true --set prometheus.serviceMonitor=true > ../kuberhealthy-prometheus-operator.yaml
