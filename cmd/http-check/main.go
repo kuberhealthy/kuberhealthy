1d07c13ef54fb1beb10b542cdd76aa08d83c4688
Jonathan Phan <jonnydawg@users.noreply.github.com>
2019-12-13 12:27:22 -0800

    Review changese
    changes for review

--- cmd/http-check/main.go
+++ cmd/http-check/main.go
@@ -37,7 +37,7 @@ func main() {
 		reportErr := fmt.Errorf("error occurred performing a " + http.MethodGet + " to " + checkURL + ": " + err.Error())
 		log.Errorln(reportErr.Error())
 		kh.ReportFailure([]string{reportErr.Error()})
-		os.Exit(0)
+		os.Exit(1)
 	}
 
 	// Check if the response status code is a 200.
