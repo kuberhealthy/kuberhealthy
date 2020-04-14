2c2af09fbb27ba2aa9b599fdc2cce6a94541190a
Edvin Norling <edvin.norling@gmail.com>
2020-04-14 14:58:27 +0200

    Uncomment ns and deploy part
    Test localy and forgot to change it back to the real config
    Sigh2

--- .ci/e2e.sh
+++ .ci/e2e.sh
@@ -9,15 +9,15 @@
 NS=kuberhealthy
 
 # Create namespace
-#kubectl create namespace $NS
+kubectl create namespace $NS
 
 # Create kuberhealthy crd etc.
-#kubectl create -f deploy/kuberhealthy.yaml
+kubectl create -f deploy/kuberhealthy.yaml
 
 # Wait for kuberhealthy operator to start
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl -n $NS get pods -l app=kuberhealthy -o jsonpath="$JSONPATH" 2>&1 |grep -q "Ready=True"; do sleep 1;echo "waiting for kuberhealthy operator to be available"; kubectl get pods -n $NS; done
 
-for i in {1..100}
+for i in {1..60}
 do
     khsCount=$(oc get -n $NS khs -o yaml |grep OK |wc -l)
 
