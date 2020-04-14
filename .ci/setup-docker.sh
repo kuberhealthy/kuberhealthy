c10b2f5ad3ae7a3f3b2d715b2e47e93c2c83e18f
Edvin Norling <edvin.norling@gmail.com>
2020-04-14 13:58:34 +0200

    Initial e2e test kuberhealthy

---
+++ .ci/setup-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## this script is meant to be executed in a CI executor based on Ubuntu 18.04 and hasn't been tested anywhere else
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
