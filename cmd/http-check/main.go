03b16d4d2cb1556bb23ec1844a1e2d834255dbde
Justin Downie <justin_downie@cable.comcast.com>
2020-04-27 13:22:09 -0700

    corrected verbage for debugging

--- cmd/http-check/main.go
+++ cmd/http-check/main.go
@@ -47,19 +47,26 @@ func main() {
 			continue
 		}
 
+		if r.StatusCode != http.StatusOK {
+			log.Println("Got a", r.StatusCode, "with a", http.MethodGet, "to", checkURL)
+			checksFailed = checksFailed + 1
+			continue
+		}
+
 		if err != nil {
 			checksFailed = checksFailed + 1
 			// log.Println("Got a", r.StatusCode, "with a", http.MethodGet, "to", checkURL)
 			log.Println("Failed to reach URL: ", checkURL, " recieved a ", r.StatusCode)
 			continue
 		}
+
 		time.Sleep(time.Second)
 	}
 
 	// Debug logging counts
-	log.Println(checksRan)
-	log.Println(checksPassed)
-	log.Println(checksFailed)
+	log.Println(checksRan, "checks ran")
+	log.Println(checksPassed, "checks passed")
+	log.Println(checksFailed, "checks failed")
 
 	// Check to see if the 10 requests passed at 80%
 	if checksPassed >= 8 {
@@ -69,17 +76,17 @@ func main() {
 			os.Exit(0)
 		}
 		log.Println("Successfully reported to Kuberhealthy")
+	}
 
-		// Kuberhealthy reporting when checks passed is less than 8 out of 10 attempts
-		if checksPassed < 8 {
-			reportErr := fmt.Errorf("unable to retrieve a http.StatusOK from " + checkURL + "check failed " + strconv.Itoa(checksFailed) + " out of 10 attempts")
-			// log.Println(reportErr.Error())
-			err := kh.ReportFailure([]string{reportErr.Error()})
-			if err != nil {
-				log.Fatalln("error when reporting to kuberhealthy:", err.Error())
-				os.Exit(0)
-			}
-			log.Println("Successfully reported to Kuberhealthy of failure")
+	// Kuberhealthy reporting when checks passed is less than 8 out of 10 attempts
+	if checksPassed < 8 {
+		reportErr := fmt.Errorf("unable to retrieve a http.StatusOK from " + checkURL + "check failed " + strconv.Itoa(checksFailed) + " out of 10 attempts")
+		// log.Println(reportErr.Error())
+		err := kh.ReportFailure([]string{reportErr.Error()})
+		if err != nil {
+			log.Fatalln("error when reporting to kuberhealthy:", err.Error())
+			os.Exit(0)
 		}
+		log.Println("Successfully reported to Kuberhealthy of failure")
 	}
 }
