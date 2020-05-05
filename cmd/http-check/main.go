d6207f1568bd1036f0f489888aefef18428e6b42
Justin Downie <justin_downie@cable.comcast.com>
2020-05-05 14:06:28 -0700

    added logic to set duration time and repititions in env variables

--- cmd/http-check/main.go
+++ cmd/http-check/main.go
@@ -13,14 +13,26 @@ import (
 )
 
 var (
-	// HTTP endpoint to send a request to.
+	// Environment Variables fetched from spec file
 	checkURL = os.Getenv("CHECK_URL")
+	count    = os.Getenv("COUNT")
+	seconds  = os.Getenv("SECONDS")
 )
 
 func init() {
 	// Check that the URL environment variable is valid.
 	if len(checkURL) == 0 {
 		log.Fatalln("Empty URL. Please update your CHECK_URL environment variable.")
+	}
+
+	// Check that the COUNT environment variable is valid.
+	if len(count) == 0 {
+		log.Fatalln("Empty count value. Please update your COUNT environemnt variable.")
+	}
+
+	// Check that the SECONDS environment variable is valid.
+	if len(seconds) == 0 {
+		log.Fatalln("Empty seconds value. Please update your SECONDS environemnt variable.")
 	}
 
 	// If the URL does not begin with HTTP, exit.
@@ -30,16 +42,28 @@ func init() {
 }
 
 func main() {
+
+	countInt, x := strconv.Atoi(count)
+	if x != nil {
+		log.Fatalln(x)
+	}
+
+	secondInt, y := strconv.Atoi(seconds)
+	if y != nil {
+		log.Fatalln(y)
+	}
+
 	log.Infoln("Beginning check.")
 	checksRan := 0
 	checksPassed := 0
 	checksFailed := 0
 
-	ticker := time.NewTicker(time.Second)
+	ticker := time.NewTicker(time.Duration(secondInt))
 	defer ticker.Stop()
 	// This for loop makes a http GET request to a known internet address, address can be changed in deployment spec yaml
 	// and returns a http status every second.
-	for checksRan < 10 {
+
+	for checksRan < countInt {
 		<-ticker.C
 		r, err := http.Get(checkURL)
 		checksRan++
