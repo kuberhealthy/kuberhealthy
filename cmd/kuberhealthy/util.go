0f88ef3d84baf88b9dd04275a23dedd2d2618dd8
joshulyne <joshpark118@gmail.com>
2020-06-02 16:43:54 -0700

    Use return instead of break

--- cmd/kuberhealthy/util.go
+++ cmd/kuberhealthy/util.go
@@ -54,7 +54,7 @@ func notifyChanLimiter(maxSpeed time.Duration, inChan chan struct{}, outChan cha
 			select {
 			case <-time.After(maxSpeed):
 				outChan <- struct{}{}
-				break
+				return
 			case <-inChan:
 				log.Println("channel notify limiter witnessed an upstream message on inChan and is waiting an additional", maxSpeed, "before sending output")
 			}
