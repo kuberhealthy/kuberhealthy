05435134cfffd8706e5547fd0e54bd7febc3d525
joshulyne <joshpark118@gmail.com>
2020-06-02 16:16:40 -0700

    fix

--- cmd/deployment-check/service.go
+++ cmd/deployment-check/service.go
@@ -23,7 +23,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/ fpkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	watchpkg "k8s.io/apimachinery/pkg/watch"
 )
 
