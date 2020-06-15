cb8c384262a29bc61f0e1081b33cfa42b4828583
Eric Greer <eric.greer@comcast.com>
2020-06-15 12:22:26 -0700

    removed broken tests. added test for watchDir

--- cmd/http-check/main.go
+++ cmd/http-check/main.go
@@ -13,14 +13,27 @@ import (
 )
 
 var (
-	// HTTP endpoint to send a request to.
+	// Environment Variables fetched from spec file
 	checkURL = os.Getenv("CHECK_URL")
+	count    = os.Getenv("COUNT")
+	seconds  = os.Getenv("SECONDS")
+	passing  = os.Getenv("PASSING")
 )
 
 func init() {
 	// Check that the URL environment variable is valid.
 	if len(checkURL) == 0 {
 		log.Fatalln("Empty URL. Please update your CHECK_URL environment variable.")
+	}
+
+	// Check that the COUNT environment variable is valid.
+	if len(count) == 0 {
+		log.Fatalln("Empty count value. Please update your COUNT environemnt variable.")
+	}
+
+	// Check that the SECONDS environment variable is valid.
+	if len(seconds) == 0 {
+		log.Fatalln("Empty seconds value. Please update your SECONDS environemnt variable.")
 	}
 
 	// If the URL does not begin with HTTP, exit.
@@ -30,35 +43,47 @@ func init() {
 }
 
 func main() {
+
+	countInt, err := strconv.Atoi(count)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	secondInt, err := strconv.Atoi(seconds)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	passingFloat, err := strconv.ParseFloat(passing, 64)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// sets the passing score to compare it against checks that have been ran
+	floatCount := float64(countInt)
+	passingScore := floatCount * passingFloat
+	passInt := int(passingScore)
+	log.Infoln("Looking for at least", passInt, "of", countInt, "checks to pass")
+
 	log.Infoln("Beginning check.")
 	checksRan := 0
 	checksPassed := 0
 	checksFailed := 0
 
-	ticker := time.NewTicker(time.Second)
+	ticker := time.NewTicker(time.Duration(secondInt))
 	defer ticker.Stop()
+
 	// This for loop makes a http GET request to a known internet address, address can be changed in deployment spec yaml
 	// and returns a http status every second.
-	for checksRan < 10 {
+	for checksRan < countInt {
 		<-ticker.C
 		r, err := http.Get(checkURL)
 		checksRan++
 
-<<<<<<< HEAD
 		if err != nil {
 			checksFailed++
 			log.Infoln("Failed to reach URL: ", checkURL)
 			continue
-=======
-	// Make a GET request.
-	r, err := http.Get(checkURL)
-	if err != nil {
-		reportErr := fmt.Errorf("error occurred performing a " + http.MethodGet + " to " + checkURL + ": " + err.Error())
-		log.Errorln(reportErr.Error())
-		reportFailureErr := kh.ReportFailure([]string{reportErr.Error()})
-		if reportFailureErr != nil {
-			log.Fatalln("error when reporting to kuberhealthy:", reportFailureErr.Error())
->>>>>>> master
 		}
 
 		if r.StatusCode != http.StatusOK {
@@ -67,7 +92,6 @@ func main() {
 			continue
 		}
 		log.Infoln("Got a", r.StatusCode, "with a", http.MethodGet, "to", checkURL)
-<<<<<<< HEAD
 		checksPassed++
 	}
 
@@ -76,31 +100,18 @@ func main() {
 	log.Infoln(checksPassed, "checks passed")
 	log.Infoln(checksFailed, "checks failed")
 
-	// Check to see if the 10 requests passed at 80% and reports to Kuberhealthy accordingly
-	if checksPassed < 8 {
-		reportErr := fmt.Errorf("unable to retrieve a valid response from " + checkURL + "check failed " + strconv.Itoa(checksFailed) + " out of 10 attempts")
+	// Check to see if the number of requests passed at 90% and reports to Kuberhealthy accordingly
+	if checksPassed < passInt {
+		reportErr := fmt.Errorf("unable to retrieve a valid response from " + checkURL + "check failed " + strconv.Itoa(checksFailed) + " out of " + strconv.Itoa(checksRan) + " attempts")
 		err := kh.ReportFailure([]string{reportErr.Error()})
 		if err != nil {
 			log.Fatalln("error when reporting to kuberhealthy:", err.Error())
 		}
-		log.Infoln("Successfully reported to Kuberhealthy of failure")
-=======
-		reportSuccessErr := kh.ReportSuccess()
-		if err != nil {
-			log.Fatalln("error when reporting to kuberhealthy:", reportSuccessErr.Error())
-		}
+		log.Infoln("Reported Failure to Kuberhealthy")
 		os.Exit(0)
 	}
 
-	reportErr := fmt.Errorf("unable to retrieve a " + strconv.Itoa(http.StatusOK) + " from " + checkURL + " got a " + strconv.Itoa(r.StatusCode) + " instead")
-	log.Errorln(reportErr.Error())
-	reportFailureErr := kh.ReportFailure([]string{reportErr.Error()})
-	if reportFailureErr != nil {
-		log.Fatalln("error when reporting to kuberhealthy:", reportFailureErr.Error())
->>>>>>> master
-	}
-
-	err := kh.ReportSuccess()
+	err = kh.ReportSuccess()
 	if err != nil {
 		log.Fatalln("error when reporting to kuberhealthy:", err.Error())
 	}
