968f450f5731a05ae28615a00c38edf0d16f793f
Jonathan Phan <jonathan_phan@comcast.com>
2020-07-08 14:51:03 -0700

    check if parsed k=v are valid

--- cmd/deployment-check/input.go
+++ cmd/deployment-check/input.go
@@ -126,10 +126,14 @@ func parseInputValues() {
 	}
 
 	// Parse incoming deployment node selectors
-	if len(checkDeploymentNodeSelectorsEnv) != 0 {
+	if len(checkDeploymentNodeSelectorsEnv) > 0 {
 		splitEnvVars := strings.Split(checkDeploymentNodeSelectorsEnv, ",")
 		for _, splitEnvVarKeyValuePair := range splitEnvVars {
 			parsedEnvVarKeyValuePair := strings.Split(splitEnvVarKeyValuePair, "=")
+			if len(parsedEnvVarKeyValuePair) != 2 {
+				log.Warnln("Unable to parse key value pair:", splitEnvVarKeyValuePair)
+				continue
+			}
 			if _, ok := checkDeploymentNodeSelectors[parsedEnvVarKeyValuePair[0]]; !ok {
 				checkDeploymentNodeSelectors[parsedEnvVarKeyValuePair[0]] = parsedEnvVarKeyValuePair[1]
 			}
