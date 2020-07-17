f697c6b5847e1719d71e61c2a71db00471f5517b
Jonathan Phan <jonathan_phan@comcast.com>
2020-07-17 16:26:14 -0700

    remove unneeded files

--- cmd/webhook/webhook-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Retrieving certificate authority data from cluster: $(kubectl config current-context)"
-CA_DATA=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}') && echo "CA_DATA set to $CA_DATA"
