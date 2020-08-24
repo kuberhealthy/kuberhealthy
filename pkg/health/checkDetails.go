c8ed5549700086ee5d60ab00e02420799534bd8b
integrii <integrii@gmail.com>
2020-08-24 15:24:27 -0700

    jobs use main kh context. getter for khworkload type on WorkloadDetails

--- pkg/health/checkDetails.go
+++ pkg/health/checkDetails.go
@@ -21,10 +21,10 @@ type WorkloadDetails struct {
 	Errors           []string
 	RunDuration      string
 	Namespace        string
-	LastRun          time.Time 		// the time the check last was last run
-	AuthoritativePod string    		// the pod that last ran the check
-	CurrentUUID      string    		`json:"uuid"` // the UUID that is authorized to report statuses into the kuberhealthy endpoint
-	KHWorkload       KHWorkload		`json:"KHWorkload,omitempty"`
+	LastRun          time.Time // the time the check last was last run
+	AuthoritativePod string    // the pod that last ran the check
+	CurrentUUID      string    `json:"uuid"` // the UUID that is authorized to report statuses into the kuberhealthy endpoint
+	khWorkload       KHWorkload
 }
 
 // NewWorkloadDetails creates a new WorkloadDetails struct
@@ -34,13 +34,7 @@ func NewWorkloadDetails() WorkloadDetails {
 	}
 }
 
-// KHWorkload is used to describe the different types of kuberhealthy workloads: KhCheck or KHJob
-type KHWorkload string
-
-// Two types of KHWorkloads are available: Kuberhealthy Check or Kuberhealthy Job
-// KHChecks run on a scheduled run interval
-// KHJobs run once
-const (
-	KHCheck KHWorkload = "KHCheck"
-	KHJob KHWorkload = "KHJob"
-)
+// GetKHWorkload returns the workload for the WorkloadDetails struct
+func (wd *WorkloadDetails) GetKHWorkload() KHWorkload {
+	return wd.khWorkload
+}
