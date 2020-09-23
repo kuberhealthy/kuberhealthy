aae3f8006de7b2269dce1604b7c159761fffe791
Justin Downie <justin_downie@cable.comcast.com>
2020-09-23 08:15:25 -0700

    created func to fetch cronjobs

--- cmd/cronjob-checker/main.go
+++ cmd/cronjob-checker/main.go
@@ -10,6 +10,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/events"
 )
 
 // kubeConfigFile is a variable containing file path of Kubernetes config files
@@ -21,7 +22,7 @@ func main() {
 		log.Fatalln("Unable to create kubernetes client", err)
 	}
 
-	// cronjobs, err :=
+	events
 
 }
 
@@ -30,11 +31,16 @@ func listCronJobs(client *kubernetes.Clientset, namespace string) (map[string]v1
 	log.Infoln("Listing CronJobs")
 
 	CronJobs := make(map[string]v1.Pod)
-
 	cj, err := client.BatchV1beta1().CronJobs(namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		log.Errorln("Failed to list CronJobs")
 		return CronJobs, err
 	}
 	log.Infoln("Found:", len(cj.Items), "cronjobs in namespace:", namespace)
+
+	cj.Descriptor()
+
+	for _, c := range cj.Items {
+		if c.GetLabels()
+	}
 }
