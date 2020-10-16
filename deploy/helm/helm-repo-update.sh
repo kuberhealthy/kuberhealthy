02b506b6b3ca71b7d972f8202127301c5fcc2665
cbitze7387k <Chad_Bitzer@cable.comcast.com>
2020-10-16 16:10:17 -0700

    fix release tag match to include zero and update 3rd party actions

--- deploy/helm/helm-repo-update.sh
+++ deploy/helm/helm-repo-update.sh
@@ -1,5 +1,5 @@
 
-#modify Chart.yaml based on whether workflow was triggered by a new tag or a file update in ./deploy/helm
+# Modify Chart.yaml based on whether workflow was triggered by a new tag or a file update in ./deploy/helm
 if [[ ${GITHUB_REF##*/} =~ "v"[0-9].*\.[0-9].*\.[0-9].* ]]; then
 	sed -i -e "s/^appVersion:.*/appVersion: ${GITHUB_REF##*/}/" ./deploy/helm/kuberhealthy/Chart.yaml
 	sed -i -e "s/^version:.*/version: $GITHUB_RUN_NUMBER/" ./deploy/helm/kuberhealthy/Chart.yaml
@@ -7,7 +7,7 @@ else
 	sed -i -e "s/^version:.*/version: $GITHUB_RUN_NUMBER/" ./deploy/helm/kuberhealthy.Chart.yaml
 fi
 
-# the github action we use has helm 3 (required) as 'helmv3' in its path, so we alias that in and use it if present
+# The github action we use has helm 3 (required) as 'helmv3' in its path, so we alias that in and use it if present
 HELM="helm"
 if which helmv3; then
     echo "Using helm v3 alias"
@@ -16,17 +16,20 @@ fi
 
 $HELM version
 
+# Lint helm chart
 $HELM lint ./kuberhealthy
 if [ "$?" -ne "0" ]; then
   echo "Linting reports error"
   exit 1
 fi
 
+# Package Helm Charts
 $HELM package --version $GITHUB_RUN_NUMBER -d ../../helm-repos/tmp.d ./kuberhealthy
 
+# Append new package info to top of helm index file
 cd ../../helm-repos
-
 $HELM repo index ./tmp.d --merge ./index.yaml --url https://comcast.github.io/kuberhealthy/helm-repos/archives
 
+# Move the newly packaged .tgz file and index.yaml where they need to be
 mv -f ./tmp.d/kuberhealthy-*.tgz ./archives
 mv -f ./tmp.d/index.yaml ./
