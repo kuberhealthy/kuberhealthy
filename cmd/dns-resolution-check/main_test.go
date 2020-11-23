73ec6288c878fb52e68525625f587e8a4f98eadf
rjacks005c <robert_jackson9@cable.comcast.com>
2020-11-23 15:17:40 -0700

    adding unit testing for createResolver, and modifying unit test for doChecks to bypass dns timeouts

--- cmd/dns-resolution-check/main_test.go
+++ cmd/dns-resolution-check/main_test.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"errors"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/Comcast/kuberhealthy/v2/pkg/kubeClient"
+	log "github.com/sirupsen/logrus"
 )
 
 func TestDoChecks(t *testing.T) {
 	dc := New()
+	client, err := kubeClient.Create(filepath.Join(os.Getenv("HOME"), ".kube", "config"))
+	if err != nil {
+		log.Fatalln("Unable to create kubernetes client", err)
+	}
+	dc.client = client
 	testCase := make(map[string]error)
 	testCase["bad.host.com"] = errors.New("DNS Status check determined that bad.host.com is DOWN")
 	testCase["google.com"] = nil
@@ -16,7 +26,10 @@ func TestDoChecks(t *testing.T) {
 		dc.Hostname = arg
 
 		err := dc.doChecks()
-
+		if strings.Contains(err.Error(), "i/o timeout") {
+			t.Logf("DNS Timed out")
+			continue
+		}
 		switch err {
 		case nil:
 			if dc.Hostname != "google.com" {
