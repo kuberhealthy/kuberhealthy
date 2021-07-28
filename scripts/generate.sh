ef94f91591763b410a7b4c4866922b9bf8808365
Eric Greer <eric.greer@comcast.com>
2021-07-28 14:46:59 -0700

    working on generate-crds.go fixes

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,10 +5,8 @@ go get github.com/brancz/gojsontoyaml
 
 GOPATH=$(go env GOPATH)
 
-go run -v ./scripts/generate-crds.go --controller-gen=${GOPATH}/bin/controller-gen --gojsontoyaml=${GOPATH}/bin/gojsontoyaml
-
-echo $(pwd)
+go run -v ./generate-crds.go --controller-gen=${GOPATH}/bin/controller-gen --gojsontoyaml=${GOPATH}/bin/gojsontoyaml
 
 ## Edit the khcheck and khjob crds files to have `preserveUnknownFields: false`
-awk '{print} sub(/scope: Namespaced/,"preserveUnknownFields: false")' ./scripts/generated/comcast.github.io_khchecks.yaml > tmpfile.yaml  && mv tmpfile.yaml ./scripts/generated/comcast.github.io_khchecks.yaml
-awk '{print} sub(/scope: Namespaced/,"preserveUnknownFields: false")' ./scripts/generated/comcast.github.io_khjobs.yaml > tmpfile.yaml  && mv tmpfile.yaml ./scripts/generated/comcast.github.io_khjobs.yaml
+awk '{print} sub(/scope: Namespaced/,"preserveUnknownFields: false")' ./generated/comcast.github.io_khchecks.yaml > tmpfile.yaml  && mv tmpfile.yaml ./generated/comcast.github.io_khchecks.yaml
+awk '{print} sub(/scope: Namespaced/,"preserveUnknownFields: false")' ./generated/comcast.github.io_khjobs.yaml > tmpfile.yaml  && mv tmpfile.yaml ./generated/comcast.github.io_khjobs.yaml
