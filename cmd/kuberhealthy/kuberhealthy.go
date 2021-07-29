d0d5cd21a87af2af3b8a4d2e7ddf4a3378a0bfdc
zhanso101 <zachary_hanson@comcast.com>
2021-07-29 09:52:15 -0400

    Updates copyright information for CNCF.

--- cmd/kuberhealthy/kuberhealthy.go
+++ cmd/kuberhealthy/kuberhealthy.go
@@ -1,14 +1,3 @@
-// Copyright 2018 Comcast Cable Communications Management, LLC
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//     http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
@@ -943,7 +932,7 @@ func (k *Kuberhealthy) runJob(ctx context.Context, job khjobv1.KuberhealthyJob) 
 	details.CurrentUUID = jobDetails.CurrentUUID
 
 	// Fetch node information from running check pod using kh run uuid
-	selector := "kuberhealthy-run-id="+details.CurrentUUID
+	selector := "kuberhealthy-run-id=" + details.CurrentUUID
 	pod, err := k.fetchPodBySelector(selector)
 	if err != nil {
 		log.Errorln(err)
@@ -1056,7 +1045,7 @@ func (k *Kuberhealthy) runCheck(ctx context.Context, c *external.Checker) {
 		details.CurrentUUID = checkDetails.CurrentUUID
 
 		// Fetch node information from running check pod using kh run uuid
-		selector := "kuberhealthy-run-id="+details.CurrentUUID
+		selector := "kuberhealthy-run-id=" + details.CurrentUUID
 		pod, err := k.fetchPodBySelector(selector)
 		if err != nil {
 			log.Errorln(err)
