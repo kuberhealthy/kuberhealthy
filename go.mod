222ea3f7ea985fa0113cd3a8714adc838e44f734
Jonathan Phan <jonathan_phan@comcast.com>
2021-07-30 11:33:16 -0700

    fix KHState CRD time field by making it nullable

--- go.mod
+++ go.mod
@@ -2,29 +2,48 @@ module github.com/kuberhealthy/kuberhealthy/v2
 
 require (
 	github.com/Pallinder/go-randomdata v1.1.0
+	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
 	github.com/aws/aws-sdk-go v1.25.24
 	github.com/blang/semver v3.5.1+incompatible // indirect
 	github.com/brancz/gojsontoyaml v0.0.0-20210709073738-09985366e724 // indirect
 	github.com/cenkalti/backoff v2.2.1+incompatible
 	github.com/codingsince1985/checksum v1.1.0
+	github.com/coreos/go-etcd v2.0.0+incompatible // indirect
+	github.com/cpuguy83/go-md2man v1.0.10 // indirect
 	github.com/denverdino/aliyungo v0.0.0-20191023002520-dba750c0c223 // indirect
+	github.com/docker/docker v0.7.3-0.20190327010347-be7ac8be2ae0 // indirect
+	github.com/docker/spdystream v0.0.0-20160310174837-449fdfce4d96 // indirect
+	github.com/fatih/color v1.12.0 // indirect
 	github.com/ghodss/yaml v1.0.0
 	github.com/go-ini/ini v1.49.0 // indirect
+	github.com/go-openapi/validate v0.19.5 // indirect
+	github.com/gobuffalo/flect v0.2.3 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/uuid v1.1.2
+	github.com/gophercloud/gophercloud v0.1.0 // indirect
 	github.com/gorhill/cronexpr v0.0.0-20180427100037-88b0669f7d75
 	github.com/influxdata/influxdb1-client v0.0.0-20190402204710-8ff2fc3824fc
 	github.com/integrii/flaggy v1.2.2
+	github.com/onsi/gomega v1.14.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/pkg/sftp v1.10.1 // indirect
-	github.com/sirupsen/logrus v1.4.2
+	github.com/sirupsen/logrus v1.7.0
 	github.com/smartystreets/goconvey v1.6.4 // indirect
-	gopkg.in/ini.v1 v1.51.0 // indirect
+	github.com/spf13/cobra v1.2.1 // indirect
+	github.com/ugorji/go v1.1.4 // indirect
+	github.com/ugorji/go/codec v0.0.0-20181204163529-d75b2dcb6bc8 // indirect
+	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
+	golang.org/x/tools v0.1.5 // indirect
 	gopkg.in/yaml.v2 v2.4.0
-	k8s.io/api v0.19.3
-	k8s.io/apimachinery v0.19.3
-	k8s.io/client-go v0.19.0
+	gotest.tools v2.2.0+incompatible // indirect
+	k8s.io/api v0.21.3
+	k8s.io/apiextensions-apiserver v0.21.3 // indirect
+	k8s.io/apimachinery v0.21.3
+	k8s.io/client-go v0.21.3
+	k8s.io/klog v1.0.0 // indirect
 	k8s.io/kops v1.11.0
-	sigs.k8s.io/controller-tools v0.4.1 // indirect
+	sigs.k8s.io/controller-tools v0.6.2 // indirect
+	sigs.k8s.io/structured-merge-diff/v3 v3.0.0 // indirect
 )
 
 go 1.15
--- pkg/apis/khstate/v1/types.go
+++ pkg/apis/khstate/v1/types.go
@@ -17,28 +17,30 @@ import (
 // +kubebuilder:resource:singular="khstate"
 // +kubebuilder:resource:shortName="khs"
 type KuberhealthyState struct {
-	metav1.TypeMeta `json:",inline"`
+	metav1.TypeMeta `json:",inline" yaml:",inline"`
 	// +optional
-	metav1.ObjectMeta `json:"metadata,omitempty"`
+	metav1.ObjectMeta `json:"metadata,omitempty" yaml:"metadata,omitempty"`
 
 	// Spec holds the desired state of the KuberhealthyState (from the client).
 	// +optional
-	Spec WorkloadDetails `json:"spec"`
+	Spec WorkloadDetails `json:"spec" yaml:"spec"`
 }
 
 // WorkloadDetails contains details about a single kuberhealthy check or job's current status
 // +k8s:openapi-gen=true
+// +nullable:name="LastRun"
 type WorkloadDetails struct {
-	OK               bool      `json:"OK"`               // true or false status of the khWorkload, whether or not it completed successfully
-	Errors           []string  `json:"Errors"`           // the list of errors reported from the khWorkload run
-	RunDuration      string    `json:"RunDuration"`      // the time it took for the khWorkload to complete
-	Namespace        string    `json:"Namespace"`        // the namespace the khWorkload was run in
-	Node 			 string    `json:"Node"`		     // the node the khWorkload ran on
-	// +optional
-	LastRun          metav1.Time `json:"LastRun"`          // the time the khWorkload was last run
-	AuthoritativePod string    `json:"AuthoritativePod"` // the main kuberhealthy pod creating and updating the khstate
-	CurrentUUID      string    `json:"uuid"`             // the UUID that is authorized to report statuses into the kuberhealthy endpoint
-	khWorkload       KHWorkload
+	OK          bool     `json:"OK" yaml:"OK"`                   // true or false status of the khWorkload, whether or not it completed successfully
+	Errors      []string `json:"Errors" yaml:"Errors"`           // the list of errors reported from the khWorkload run
+	RunDuration string   `json:"RunDuration" yaml:"RunDuration"` // the time it took for the khWorkload to complete
+	Namespace   string   `json:"Namespace" yaml:"Namespace"`     // the namespace the khWorkload was run in
+	Node        string   `json:"Node" yaml:"Node"`               // the node the khWorkload ran on
+	// +nullable
+	LastRun          *metav1.Time `json:"LastRun,omitempty" yaml:"LastRun,omitempty"` // the time the khWorkload was last run
+	AuthoritativePod string       `json:"AuthoritativePod" yaml:"AuthoritativePod"`   // the main kuberhealthy pod creating and updating the khstate
+	CurrentUUID      string       `json:"uuid" yaml:"uuid"`                           // the UUID that is authorized to report statuses into the kuberhealthy endpoint
+	// +nullable
+	khWorkload *KHWorkload `json:"khWorkload,omitempty" yaml:"khWorkload,omitempty"`
 }
 
 // KHWorkload is used to describe the different types of kuberhealthy workloads: KhCheck or KHJob
@@ -56,8 +58,8 @@ const (
 
 // KuberhealthyStateList is a list of KuberhealthyState resources
 type KuberhealthyStateList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata"`
+	metav1.TypeMeta `json:",inline" yaml:",inline"`
+	metav1.ListMeta `json:"metadata" yaml:"metadata"`
 
-	Items []KuberhealthyState `json:"items"`
+	Items []KuberhealthyState `json:"items" yaml:"items"`
 }
