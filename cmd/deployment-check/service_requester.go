3c5956592aad6f3bcb1845fc54b7af7c970b486f
zhicheng.li <zhicheng.li@trip.com>
2022-03-01 17:49:51 +0800

    The correction for deploymen-check code
    Signed-off-by: zhicheng.li <zhicheng.li@trip.com>

--- cmd/deployment-check/service_requester.go
+++ cmd/deployment-check/service_requester.go
@@ -47,7 +47,7 @@ func makeRequestToDeploymentCheckService(ctx context.Context, hostname string) c
 		// Try to make requests to the hostname endpoint and wait for a result.
 		select {
 		case result := <-getRequestBackoff(hostname):
-			if &result == nil {
+			if result == (RequestResult{}) {
 				requestChan <- errors.New("got a nil request result from the backoff process")
 				return
 			}
