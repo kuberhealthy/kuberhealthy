8915f63bb26edd70c0b7105c7b2f85554e60dc51
Michael Kurashima <michael.kurashima@comcast.com>
2022-05-24 20:56:21 -0700

    ditch containerd due to complexities having to run a client/server

--- cmd/image-download-check/main.go
+++ cmd/image-download-check/main.go
@@ -1,76 +1,88 @@
 package main
 
+import (
+	"context"
+
+	"github.com/containerd/containerd"
+	log "github.com/sirupsen/logrus"
+)
+
 func main() {
 
-	/*
+	var imageName = "nginx"
 
-			c := config.New()
-
-			s := plugin.NewStore()
-
-			ctx := context.Background()
-
-			d, err := daemon.NewDaemon(ctx, c, s)
-			if err != nil {
-				log.Fatalln("error creating new docker daemon", err)
-			}
-
-			image, err := d.ImageService().LookupImage("nginx")
-			if err != nil {
-				log.Fatalln("error looking up image", err)
-			}
-
-			b := new(bytes.Buffer)
-
-			d.ImageService().PullImage(ctx, image.Config.Image, "", nil, nil, nil, b)
-
-
-				// TODO: make this run from a docker server instead of a client
-				// make a local docker client
-				endpoint := "unix:///var/run/docker.sock"
-				dockerClient, err := docker.NewClient(endpoint)
-				if err != nil {
-					log.Errorln("failed to created new docker client", err)
-					os.Exit(1)
-				}
-
-				// create a bytes buffer for our docker pull options output stream
-				b := new(bytes.Buffer)
-
-				// create channel that will read bytes from our buffer
-				// c := make(chan bytes.Buffer)
-
-				// set options/auth for docker image pull
-				opts := docker.PullImageOptions{
-					Repository:   "nginx",
-					Tag:          "1.21",
-					OutputStream: b,
-				}
-				auth := docker.AuthConfiguration{}
-
-				// perform docker pull on target image
-				pullImage(dockerClient, opts, auth)
-
-				fmt.Println(b.Len())
+	err := pullImage(imageName)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// timeTrack tracks the time it takes for a function to complete
-	func timeTrack(start time.Time, name string) time.Duration {
-		elapsed := time.Since(start)
-		log.Printf("%s took %s", name, elapsed)
-		return elapsed
-	}
-
-	func pullImage(dockerClient *docker.Client, opts docker.PullImageOptions, auth docker.AuthConfiguration) {
-		// start timeTrack timer
-		defer timeTrack(time.Now(), "pullImage")
-
-		// pull image
-		err := dockerClient.PullImage(opts, auth)
-		if err != nil {
-			log.Errorln("failed to pull image ", err)
-			os.Exit(1)
-		}
-
-	*/
 }
+
+// pullImage pulls an image from dockerhub
+func pullImage(imageName string) error {
+
+	// create a new containerd client
+	client, err := containerd.New("/run/containerd/containerd.sock")
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	// ctx := namespaces.WithNamespace(context.Background(), "example")
+	ctx := context.Background()
+
+	image, err := client.Pull(ctx, imageName)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Successfully pulled %s image\n", image.Name())
+	log.Println("Image: ", image)
+
+	return nil
+}
+
+/*
+func listImagesDocker() error {
+
+	// create a context for docker
+	docker := namespaces.WithNamespace(context.Background(), "docker")
+
+	// set docker as the client's default namespace
+	// client, err := containerd.New("unix:///var/run/docker.sock", containerd.WithDefaultNamespace("docker"))
+	client, err := containerd.New("/var/run/docker.sock", containerd.WithDefaultNamespace("docker"))
+	if err != nil {
+		return err
+	}
+
+	// get a list of images from local docker
+	images, err := client.ListImages(docker)
+	if err != nil {
+		return err
+	}
+
+	for _, image := range images {
+		log.Println("Image::", image.Name())
+	}
+	return nil
+}
+
+func listImages() error {
+
+	client, err := containerd.New("/run/containerd/containerd.sock")
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+	ctx := namespaces.WithNamespace(context.Background(), "example")
+	images, err := client.ListImages(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, image := range images {
+		log.Println("Image::", image.Name())
+	}
+	return nil
+}
+*/
