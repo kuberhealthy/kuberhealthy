f646a9adc19443d0764a3e71fe7167560df69bd7
Eric Greer <eric.greer@epicgames.com>
2024-04-21 01:10:09 -0700

    re-add register and deepcopy files for api types

---
+++ pkg/apis/khcheck/v1/register.go
@@ -0,0 +1,31 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/kubernetes/scheme"
+)
+
+// SchemeGroupVersion variable for newly added kh checks to be added to Kuberhealthy
+var SchemeGroupVersion schema.GroupVersion
+
+// ConfigureScheme configures the runtime scheme for use with CRD creation
+func ConfigureScheme(GroupName string, GroupVersion string) error {
+	SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: GroupVersion}
+	var (
+		SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+		AddToScheme   = SchemeBuilder.AddToScheme
+	)
+	return AddToScheme(scheme.Scheme)
+}
+
+// Adds the list of known types to Scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&KuberhealthyCheck{},
+		&KuberhealthyCheckList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
--- pkg/apis/khstate/v1/types.go
+++ pkg/apis/khstate/v1/types.go
@@ -4,14 +4,25 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
 // KuberhealthyState represents the data in the CRD for configuring an
 // the state of khjobs or khchecks for Kuberhealthy
 // +k8s:openapi-gen=true
-// +genclient
+// +kubebuilder:printcolumn:name="OK",type=string,JSONPath=`.spec.OK`,description="OK status"
+// +kubebuilder:printcolumn:name="Age LastRun",type=date,JSONPath=`.spec.LastRun`,description="Last Run"
+// +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`,description="Age"
+// +kubebuilder:resource:path="khstates"
+// +kubebuilder:resource:singular="khstate"
+// +kubebuilder:resource:shortName="khs"
 type KuberhealthyState struct {
-	metav1.TypeMeta   `json:",inline" yaml:",inline"`
+	metav1.TypeMeta `json:",inline" yaml:",inline"`
+	// +optional
 	metav1.ObjectMeta `json:"metadata,omitempty" yaml:"metadata,omitempty"`
+
 	// Spec holds the desired state of the KuberhealthyState (from the client).
+	// +optional
 	Spec WorkloadDetails `json:"spec" yaml:"spec"`
 }
 
@@ -19,15 +30,17 @@ type KuberhealthyState struct {
 // +k8s:openapi-gen=true
 // +nullable:name="LastRun"
 type WorkloadDetails struct {
-	OK               bool         `json:"OK" yaml:"OK"`                               // true or false status of the khWorkload, whether or not it completed successfully
-	Errors           []string     `json:"Errors" yaml:"Errors"`                       // the list of errors reported from the khWorkload run
-	RunDuration      string       `json:"RunDuration" yaml:"RunDuration"`             // the time it took for the khWorkload to complete
-	Namespace        string       `json:"Namespace" yaml:"Namespace"`                 // the namespace the khWorkload was run in
-	Node             string       `json:"Node" yaml:"Node"`                           // the node the khWorkload ran on
+	OK          bool     `json:"OK" yaml:"OK"`                   // true or false status of the khWorkload, whether or not it completed successfully
+	Errors      []string `json:"Errors" yaml:"Errors"`           // the list of errors reported from the khWorkload run
+	RunDuration string   `json:"RunDuration" yaml:"RunDuration"` // the time it took for the khWorkload to complete
+	Namespace   string   `json:"Namespace" yaml:"Namespace"`     // the namespace the khWorkload was run in
+	Node        string   `json:"Node" yaml:"Node"`               // the node the khWorkload ran on
+	// +nullable
 	LastRun          *metav1.Time `json:"LastRun,omitempty" yaml:"LastRun,omitempty"` // the time the khWorkload was last run
 	AuthoritativePod string       `json:"AuthoritativePod" yaml:"AuthoritativePod"`   // the main kuberhealthy pod creating and updating the khstate
 	CurrentUUID      string       `json:"uuid" yaml:"uuid"`                           // the UUID that is authorized to report statuses into the kuberhealthy endpoint
-	KHWorkload       *KHWorkload  `json:"khWorkload,omitempty" yaml:"khWorkload,omitempty"`
+	// +nullable
+	khWorkload *KHWorkload `json:"khWorkload,omitempty" yaml:"khWorkload,omitempty"`
 }
 
 // KHWorkload is used to describe the different types of kuberhealthy workloads: KhCheck or KHJob
