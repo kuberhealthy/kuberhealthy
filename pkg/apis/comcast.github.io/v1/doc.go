45356ee6253bc646d3ab1840646c3d547847b3f5
Eric Greer <eric.greer@epicgames.com>
2024-12-27 16:18:47 -0800

    remove old crd gen stuff

--- pkg/apis/comcast.github.io/v1/doc.go
+++
@@ -1,5 +0,0 @@
-// +k8s:deepcopy-gen=package
-// +groupName=comcast.github.io
-
-// Package v1 is the v1 version of the khcheck API.
-package v1 // import "github.com/kuberhealthy/kuberhealthy/v2/pkg/apis/comcast.github.io/v1""
