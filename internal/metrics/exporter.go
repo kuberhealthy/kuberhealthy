eb1a5ae38f51336f9527ad8d5ac97ce05ae192df
integrii <integrii@gmail.com>
2025-03-10 00:48:15 -0700

    embrace internal dir

--- internal/metrics/exporter.go
+++ internal/metrics/exporter.go
@@ -8,7 +8,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
-	"github.com/kuberhealthy/kuberhealthy/v3/pkg/health"
+	"github.com/kuberhealthy/kuberhealthy/v3/internal/health"
 )
 
 type PromMetricsConfig struct {
