5b7becd3e9f94d1900f068d6309ed0b9f97b5c6d
integrii <integrii@gmail.com>
2025-03-17 04:18:09 -0700

    fixing package build problems. controller imports kuberhealthy struct after kuberhealthy has been started

--- internal/kuberhealthy/kuberhealthy.go
+++ internal/kuberhealthy/kuberhealthy.go
@@ -2,14 +2,13 @@ package kuberhealthy
 
 import (
 	"context"
-	"fmt"
 	"log"
-
-	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
 // Kuberhealthy handles background processing for checks
-type Kuberhealthy struct{}
+type Kuberhealthy struct {
+	Context context.Context
+}
 
 // New creates a new Kuberhealthy instance
 func New() (*Kuberhealthy, error) {
@@ -31,26 +30,6 @@ func (kh *Kuberhealthy) StopCheck(namespace string, name string) {
 // Start starts a new Kuberhealthy manager (this is the thing that kubebuilder makes)
 // along with other various processes needed to manager Kuberhealthy checks.
 func (kh *Kuberhealthy) Start(ctx context.Context) error {
-	// Create a new manager from kubernetes
-	mgr, err := ctrl.NewManager(config.GetConfigOrDie(), ctrl.Options{})
-	if err != nil {
-		return fmt.Errorf("kuberhealthy: failed to create manager:", err)
-	}
-
-	// Initialize and register the kuberhealthy controller
-	khController, err := khcontroller.New(mgr, kh)
-	if err != nil {
-		return fmt.Errorf("kuberhealthy: failed to register controller with manager:", err)
-	}
-
-	// Start the manager in a goroutine
-	go func() {
-		log.Infoln("kuberhealthy: Starting manager...")
-		err := mgr.Start(ctx)
-		if err != nil {
-			return fmt.Errorf("kuberhealthy: Manager stopped with error:", err)
-		}
-	}()
 
 	return nil
 }
