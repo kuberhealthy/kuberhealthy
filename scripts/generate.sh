66883fa7ec768ecf088da47d191ecce48217d20b
Zain Siddiqui <siddiqui.zain1@gmail.com>
2025-08-20 12:48:55 +0500

    chore: remove deprecated crd field preserveUnknownFields
    Signed-off-by: Zain Siddiqui <siddiqui.zain1@gmail.com>

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -6,7 +6,3 @@ go get github.com/brancz/gojsontoyaml
 GOPATH=$(go env GOPATH)
 
 go run -v ./generate-crds.go --controller-gen=${GOPATH}/bin/controller-gen --gojsontoyaml=${GOPATH}/bin/gojsontoyaml
-
-## Edit the khcheck and khjob crds files to have `preserveUnknownFields: false`
-awk '{print} sub(/scope: Namespaced/,"preserveUnknownFields: false")' ./generated/comcast.github.io_khchecks.yaml > tmpfile.yaml  && mv tmpfile.yaml ./generated/comcast.github.io_khchecks.yaml
-awk '{print} sub(/scope: Namespaced/,"preserveUnknownFields: false")' ./generated/comcast.github.io_khjobs.yaml > tmpfile.yaml  && mv tmpfile.yaml ./generated/comcast.github.io_khjobs.yaml
