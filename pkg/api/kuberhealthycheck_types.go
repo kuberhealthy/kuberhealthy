91f3d9ae18fd509fbc0610ff8a89c30d8e04c13e
Eric Greer <ericgreer@gmail.com>
2025-09-01 21:02:14 -0700

    Remove preserve unknown fields from check spec

--- pkg/api/kuberhealthycheck_types.go
+++ pkg/api/kuberhealthycheck_types.go
@@ -29,7 +29,6 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
-// +kubebuilder:validation:XPreserveUnknownFields
 // KuberhealthyCheckSpec defines the desired state of KuberhealthyCheck
 type KuberhealthyCheckSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
